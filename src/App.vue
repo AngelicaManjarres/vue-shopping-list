3cfeab0ec3230b76fd0fc035074c3ab78b2e750b
unknown <angelica_maria91@hotmail.com>
2022-06-05 15:14:18 -0500

    Finished shopping-list. Later on I should add update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <h3>Shopping List</h3>
+      <hr />
+      <form action="">
+        <input v-model="itemName" type="text" placeholder="Shop item">
+        <button @click="addItem">Add Item</button>
+      </form>
+      <ul>
+        <li @dblclick="deleteItem(item.id)" v-for="item in items" :key="item.id">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      items: [],
+      itemName: '',
+      url: 'http://localhost:3000/items'
+
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(this.url);
+      this.items = res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  methods: {
+    async addItem() {
+      try {
+        const res = await axios.post(this.url, {name: this.itemName})
+        this.items = [...this.items, res.data]
+        this.itemName = ''
+      }
+      catch(err) {
+        console.log(err)
+      }
+      
+    },
+
+    deleteItem(id) {
+      axios.delete(`${this.url}/${id}`)
+      this.items = this.items.filter(item => item.id !== id)
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -20,7 +64,23 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.container {
+  width: 200px;
+  margin: 0 auto;
+}
+
+ul {
+  text-align: left;
+}
+
+button {
+  background : #d5d3ec;
+  border: none;
+  padding: 10px 15px;
+  margin-top: 5px;
+}
+
+
 </style>
